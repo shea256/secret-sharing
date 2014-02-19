f8516b0df07e5e4d8b8c69b2cf3cd9e77bb2b8bb
rxl <me@ryanshea.org>
2014-02-19 17:26:09 -0500

    fix non-uniform sampling bug in randint function

--- secretsharing/utils.py
+++ secretsharing/utils.py
@@ -22,28 +22,29 @@ def get_entropy(numbytes):
     else:
         return dev_random_entropy(numbytes)
 
-def fit_num_in_range(num, lower_bound, upper_bound):
-    """ Fits the number so that it is greater than or equal to the lower bound
-        and less than the upper bound.
-    """
-    assert(isinstance(upper_bound, (int, long))
-        and isinstance(lower_bound, (int, long)) and upper_bound > lower_bound)
-    value_range = upper_bound - lower_bound
-    offset = num % value_range
-    return offset + lower_bound
-
 def randint(min_value, max_value):
     """ Chooses a random integer between min_value and max_value, inclusive.
         Range of values: [min_value, max_value]
     """
     if not (isinstance(min_value, int) and isinstance(min_value, int)):
         raise ValueError('min and max must be integers')
-    value_range = max_value - min_value
-    numbytes_of_entropy = int(ceil(value_range.bit_length()/8.0))
-    entropy = get_entropy(numbytes_of_entropy)
-    rand_int = int(entropy.encode('hex'), 16)
-    fitted_int = fit_num_in_range(rand_int, min_value, max_value+1)
-    return fitted_int
+    # Bounds are inclusive, so add 1 to the spread between the min and max
+    value_range = (max_value - min_value) + 1
+    # The bytes of entropy required depends on the bit length of the value range
+    numbytes_of_entropy = int(ceil(value_range.bit_length()/8.0)) + 1
+    # The entropy value range is the # of possible values of the entropy sample
+    entropy_value_range = 2**(numbytes_of_entropy*8)
+    # Any number greater than a multiple of the value range will be rejected
+    acceptable_sample_range = entropy_value_range - (entropy_value_range % value_range)
+    # Rejection sampling: Keep picking random #s until one falls in the range
+    while True:
+        byte_from_entropy = get_entropy(numbytes_of_entropy)
+        int_from_entropy = int(byte_from_entropy.encode('hex'), 16)
+        if int_from_entropy <= acceptable_sample_range:
+            break
+    # Take the sampled int and extract an int that's within the provided bounds
+    rand_int = min_value + (int_from_entropy % value_range)
+    return rand_int
 
 def egcd(a, b):
     if a == 0:
