e625ac19642eb4cea98380359c8887cc979c7710
rxl <me@ryanshea.org>
2014-03-17 09:30:52 -0400

    update version

--- secretsharing/__init__.py
+++ secretsharing/__init__.py
@@ -7,6 +7,6 @@
     :license: MIT, see LICENSE for more details.
 """
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .shamir import Secret
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='secretsharing',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/halfmoonlabs/secretsharing',
     license='MIT',
     author='Halfmoon Labs',
