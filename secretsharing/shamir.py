0269d60c281b724d4f2645ce547c6305658a3b53
Michael Flaxman <mflaxman@gmail.com>
2014-03-30 16:13:52 -0700

    cleanup codebase, add support for integers and the NATO alphabet

--- secretsharing/shamir.py
+++ secretsharing/shamir.py
@@ -8,50 +8,76 @@
 """
 
 import string
-from characters.charset import charset_to_int, int_to_charset
-from characters.hex import hex_to_int, int_to_hex, is_hex
-from characters.b58 import b58_to_int, int_to_b58, is_b58, B58_CHARS
+from characters import (charset_to_int, int_to_charset, is_int,
+                        hex_to_int, int_to_hex, is_hex,
+                        b58_to_int, int_to_b58, is_b58, B58_CHARS,
+                        nato_to_int, int_to_nato, is_nato, NATO_CHARS)
 
 from .utils import get_large_enough_prime, random_polynomial, \
     get_polynomial_points, modular_lagrange_interpolation
 
+def assert_share_enc_valid(share_enc):
+    assert share_enc in ('hex', 'b58', 'int', 'nato'), share_enc
+
+
 def share_to_point(share, share_enc='hex'):
     '''
     share should be in the format:
-      `01-d051080de7...` for hex
-      `2-FIXMEFIXME...` for b58
+      `01-d051080de...` for hex
+      `1-2130008653...` for int
+      `2-3AwSUjLj59...` for b58
+      `2-3ciz388ixs...` for nato
     '''
-    assert share_enc in ('hex', 'b58'), share_enc
+    assert_share_enc_valid(share_enc)
     if isinstance(share, str) and share.count('-') == 1:
-        x,y = share.split('-')
+        x, y = share.split('-')
         if share_enc == 'hex':
             if is_hex(x) and is_hex(y):
                 return (hex_to_int(x), hex_to_int(y))
         elif share_enc == 'b58':
             if is_b58(x) and is_b58(y):
                 return (b58_to_int(x), b58_to_int(y))
+        elif share_enc == 'int':
+            if is_int(x) and is_int(y):
+                return (int(x), int(y))
+        elif share_enc == 'nato':
+            if is_nato(x) and is_nato(y):
+                return (nato_to_int(x), nato_to_int(y))
     raise ValueError('Share format is invalid.')
+
 
 def point_to_share(point, share_enc='enc'):
     '''
     point should be in the format (1, 4938573982723...)
     '''
-    assert share_enc in ('hex', 'b58'), share_enc
+    assert_share_enc_valid(share_enc)
     if isinstance(point, tuple) and len(point) == 2:
-        if isinstance(point[0], (int, long)) and isinstance(point[1], (int, long)):
-            x,y = point
+        if isinstance(point[0], (int, long)) and isinstance(point[1],
+                                                            (int, long)):
+            x, y = point
             if x > 255:
-                raise ValueError('The largest x coordinate for a share is 255.')
+                msg = 'The largest x coordinate for a share is 255.'
+                raise ValueError(msg)
+
             if share_enc == 'hex':
-                hex_x, hex_y = int_to_hex(x).zfill(2), int_to_hex(y)
-                return hex_x + '-' + hex_y
+                clean_x = int_to_hex(x).zfill(2)
+                clean_y = int_to_hex(y)
             elif share_enc == 'b58':
-                # TODO: add zfill to support greater #s of shares than 58
-                b58_x, b58_y = int_to_b58(x), int_to_b58(y)
-                return b58_x + '-' + b58_y
-        else:
-            print "ah!"
+                clean_x = int_to_b58(x)
+                clean_y = int_to_b58(y)
+            elif share_enc == 'int':
+                clean_x = x
+                clean_y = y
+            elif share_enc == 'nato':
+                clean_x = int_to_nato(x)
+                clean_y = int_to_nato(y)
+            else:
+                raise ValueError('No matching share_enc found')
+
+            return '%s-%s' % (clean_x, clean_y)
+
     raise ValueError('Point format is invalid. Must be a pair of integers.')
+
 
 class Secret():
     def __init__(self, secret_int):
@@ -66,7 +92,8 @@ class Secret():
         if not isinstance(charset, str):
             raise ValueError("Charset must be a string.")
         if (set(secret) - set(charset)):
-            raise ValueError("Secret contains characters that aren't in the charset.")
+            msg = "Secret contains characters that aren't in the charset."
+            raise ValueError(msg)
         secret_int = charset_to_int(secret, charset)
         return cls(secret_int)
 
@@ -84,7 +111,7 @@ class Secret():
 
     @classmethod
     def from_shares(cls, shares, share_enc='hex'):
-        assert share_enc in ('hex', 'b58'), share_enc
+        assert_share_enc_valid(share_enc)
         if not isinstance(shares, list):
             raise ValueError("Shares must be in list form.")
         for share in shares:
@@ -102,19 +129,20 @@ class Secret():
     def split(self, threshold, num_shares, share_enc='hex'):
         '''
         Split the secret into shares.
+        The threshold is the total # of shares required to recover the secret.
 
-        The threshold is the total number of shares required to recover the secret.
+        Currently, you can return shares in hex, int, or b58 formats.
+        Feel free to add your own.
         '''
-        assert share_enc in ('hex', 'b58'), share_enc
+        assert_share_enc_valid(share_enc)
         if threshold < 2:
             raise ValueError("Threshold must be >= 2.")
         if threshold > num_shares:
             raise ValueError("Threshold must be < the total number of shares.")
-        if share_enc == 'hex':
-            assert threshold <= len(B58_CHARS), threshold
         prime = get_large_enough_prime([self._secret, num_shares])
         if not prime:
-            raise ValueError("Error! Secret is too long for share calculation!")
+            msg = "Error! Secret is too long for share calculation!"
+            raise ValueError(msg)
         coefficients = random_polynomial(threshold-1, self._secret, prime)
         points = get_polynomial_points(coefficients, num_shares, prime)
         shares = []
@@ -134,6 +162,8 @@ class Secret():
     def as_b58(self):
         return self.as_charset(B58_CHARS)
 
+    def as_nato(self):
+        return self.as_charset(NATO_CHARS)
+
     def as_printable_ascii(self):
         return self.as_charset(string.printable)
-
