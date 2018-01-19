fcab1f36c9e28d47ddea52ede01f287ed31f4574
Michael Löffler <michael@loeffler.io>
2018-01-19 17:00:45 +0200

    Simplify primes

--- secretsharing/primes.py
+++ secretsharing/primes.py
@@ -8,9 +8,8 @@
 """
 
 
-def calculate_mersenne_primes():
-    """ Returns all the mersenne primes with less than 500 digits.
-        All primes:
+def mersenne_primes(pmax=35):
+    """ Returns mersenne primes up to the given exponent number:
         3, 7, 31, 127, 8191, 131071, 524287, 2147483647L, 2305843009213693951L,
         618970019642690137449562111L, 162259276829213363391578010288127L,
         170141183460469231731687303715884105727L,
@@ -18,35 +17,27 @@ def calculate_mersenne_primes():
         53113799281676709868...70835393219031728127L, (183 digits)
         10407932194664399081...20710555703168729087L, (386 digits)
     """
-    mersenne_prime_exponents = [
-        2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279
+    exponents = [
+        2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279,  2203, 2281,
+        3217, 4253, 4423, 9689, 9941, 11213, 19937, 21701, 23209, 44497, 86243,
+        110503, 132049, 216091, 756839, 859433, 1257787, 1398269, 2976221, 3021377,
+        6972593, 13466917, 20996011, 24036583, 25964951, 30402457, 32582657, 37156667,
+        42643801, 43112609, 57885161, 74207281
     ]
-    primes = []
-    for exp in mersenne_prime_exponents:
-        prime = 1
-        for i in range(exp):
-            prime *= 2
-        prime -= 1
-        primes.append(prime)
-    return primes
+    return [2**exp - 1 for exp in exponents[:pmax]]
 
-SMALLEST_257BIT_PRIME = (2**256 + 297)
-SMALLEST_321BIT_PRIME = (2**320 + 27)
-SMALLEST_385BIT_PRIME = (2**384 + 231)
-STANDARD_PRIMES = calculate_mersenne_primes() + [
-    SMALLEST_257BIT_PRIME, SMALLEST_321BIT_PRIME, SMALLEST_385BIT_PRIME
-]
-STANDARD_PRIMES.sort()
+SP_257BIT = 2**256 + 297
+SP_321BIT = 2**320 + 27
+SP_385BIT = 2**384 + 231
+STANDARD_PRIMES = sorted(mersenne_primes() + [SP_257BIT, SP_321BIT, SP_385BIT])
 
 
-def get_large_enough_prime(batch):
+def get_large_enough_prime(batch, primes=STANDARD_PRIMES):
     """ Returns a prime number that is greater all the numbers in the batch.
     """
-    # build a list of primes
-    primes = STANDARD_PRIMES
     # find a prime that is greater than all the numbers in the batch
     for prime in primes:
-        numbers_greater_than_prime = [i for i in batch if i > prime]
-        if len(numbers_greater_than_prime) == 0:
+        numbers_greater_than_prime = sum(1 for i in batch if i > prime)
+        if not numbers_greater_than_prime:
             return prime
     return None
